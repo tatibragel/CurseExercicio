d5b1f5d6ce22c72bd833ef3d523d6c03eb14c7db
tatibragel <tbrviana@hotmail.com>
2021-02-25 11:10:02 -0300

    first commit

---
+++ CurseExercicio/Program.cs
@@ -0,0 +1,58 @@
+﻿//Fazer um programa para ler o caminho de um arquivo .csv
+//contendo os dados de itens vendidos.Cada item possui um
+//nome, preço unitário e quantidade, separados por vírgula. Você
+//deve gerar um novo arquivo chamado "summary.csv", localizado
+//em uma subpasta chamada "out" a partir da pasta original do
+//arquivo de origem, contendo apenas o nome e o valor total para
+//aquele item(preço unitário multiplicado pela quantidade),
+//conforme exemplo.
+
+using Course.Entities;
+using System;
+using System.Globalization;
+using System.IO;
+
+namespace CurseExercicio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter file full path: ");
+            string sourceFilePath = Console.ReadLine();
+
+            try
+            {
+                string[] lines = File.ReadAllLines(sourceFilePath);
+
+                string sourceFolderPath = Path.GetDirectoryName(sourceFilePath);
+                string targetFolderPath = sourceFolderPath + @"\out";
+                string targetFilePath = targetFolderPath + @"\summary.csv";
+
+                Directory.CreateDirectory(targetFolderPath);
+
+                using (StreamWriter sw = File.AppendText(targetFilePath))
+                {
+                    foreach (string line in lines)
+                    {
+
+                        string[] fields = line.Split(',');
+                        string name = fields[0];
+                        double price = double.Parse(fields[1], CultureInfo.InvariantCulture);
+                        int quantity = int.Parse(fields[2]);
+
+                        Product prod = new Product(name, price, quantity);
+
+                        sw.WriteLine(prod.Name + "," + prod.Total().ToString("F2", CultureInfo.InvariantCulture));
+                    }
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An error occurred");
+                Console.WriteLine(e.Message);
+            }
+
+        }
+    }
+}
